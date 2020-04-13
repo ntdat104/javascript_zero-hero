214a272b5cbce22b2990115b435d7ca42bd6b885
Tiến Đạt <47631594+ntdat104@users.noreply.github.com>
2020-04-13 20:21:00 +0700

    add lession 16,17

---
+++ 16-new-keyword.js
@@ -0,0 +1,58 @@
+console.log("Bài 10: Sử dụng key word 'new'");
+
+var today = new Date();
+console.log("Today =", today);
+
+//! nomal function
+var mouse = {
+  weight: 0.2,
+  getWeight: function () {
+    return this.weight;
+  },
+};
+
+//! contructor function
+//* function Student(){ };
+//* var student_1 = new Student();
+function Student(name, age) {
+  this.job = "student";
+  this.name = name;
+  this.age = age;
+}
+
+//! use "new"
+var student_1 = new Student("Dat", 20);
+var student_2 = new Student("Quynh", 21);
+var student_3 = new Student("Tuan", 21);
+var student_4 = new Student("Tien", 20);
+
+console.log("student_1 =", student_1);
+console.log("student_2 =", student_2);
+console.log("student_3 =", student_3);
+console.log("student_4 =", student_4);
+
+//TODO Thực hành mèo và chuột
+var cat = {
+  name: "Tom",
+  stomach: [],
+  eat: function (mouse) {
+    this.stomach.push(mouse);
+    return this;
+  },
+};
+
+function setMouse(name, color, age) {
+  this.name = name;
+  this.color = color;
+  this.age = age;
+}
+
+var jerry1 = new setMouse("jerry1", "Màu Xanh", 20);
+var jerry2 = new setMouse("jerry2", "Màu Đỏ", 25);
+var jerry3 = new setMouse("jerry3", "Màu Vàng", 30);
+
+cat.eat(jerry1).eat(jerry2).eat(jerry3);
+//* Gọi nhiều lần trong 1 object bởi vì return this.
+//! Method chaining.
+
+console.log("Tom = ", cat);
---
+++ 17-prototype.js
@@ -0,0 +1,32 @@
+console.log("Prototypes trong Javascript (Nguyên mẫu trong Javascript)");
+
+function Mouse(color, weight) {
+  this.type = "mouse";
+  this.color = color;
+  this.weight = weight;
+  /*
+   * this.sleep = function(){
+   *     console.log("Sleeping ...");
+   * }
+   */
+}
+console.log(Mouse);
+console.log(Mouse.prototype);
+console.log(Mouse.prototype.constructor);
+console.log(Mouse.prototype.constructor === Mouse);
+
+Mouse.prototype.sleep = function () {
+  //* dùng cách này tiết kiệm bộ nhớ hơn
+  console.log(this.color, "Sleeping ...");
+};
+
+console.log(Mouse);
+
+var jerry = new Mouse("orange", 20);
+console.log("jerry =", jerry);
+jerry.sleep();
+
+var mickey = new Mouse("white", 10);
+mickey.sleep();
+
+console.log(jerry.sleep === mickey.sleep);
